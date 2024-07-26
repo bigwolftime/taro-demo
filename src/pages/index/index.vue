e4b82fccd942e3c80c7518e017fd07921d1d0520
liuxin <liuxin@xueqiu.com>
2024-07-26 15:03:24 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="demo">
+    <view>
+      欢迎使用 NutUI 开发 Taro 多端项目 <Dongdong />
+    </view>
+    <view>{{ count }}</view>
+    <view>
+      <nut-button type="primary" @click="handleClick">
+        Count++
+      </nut-button>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Dongdong } from '@nutui/icons-vue-taro';
+import Taro from "@tarojs/taro";
+
+const count = ref(0);
+
+const handleClick = () => {
+  count.value++;
+  Taro.showToast({
+    title: `Count: ${count.value}`,
+    icon: 'none'
+  });
+};
+</script>
+
+<style>
+.demo {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
